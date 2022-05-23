d89a515fc9097664e8c737ed63b5f9ae52a3df48
luis <luis@gmail.com>
2022-05-23 14:08:10 -0500

    hagd

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: 'XXXXXXXXXXXXXXXX',
-    authDomain: 'xxxxxxxxxxxxxxx.firebaseapp.com',
-    databaseURL: 'https://xxxxxxxxxxxxxxx.firebaseio.com',
-    projectId: 'xxxxxxxxxxxxxxx',
-    storageBucket: 'xxxxxxxxxxxxxxx.appspot.com',
-    messagingSenderId: 'xxxxxxxxx',
-    appId: '1:xxxxxxxxx:web:xxxxxxxxxxx',
+    apiKey: "AIzaSyCDTLRXSaYU_4xSV4adgmRxZ70faTWkXpU",
+  authDomain: "login-ng-cc519.firebaseapp.com",
+  databaseURL: "https://login-ng-cc519-default-rtdb.firebaseio.com",
+  projectId: "login-ng-cc519",
+  storageBucket: "login-ng-cc519.appspot.com",
+  messagingSenderId: "377955233608",
+  appId: "1:377955233608:web:744c1d311d6fe81ad14d7d",
   },
 };
 
